8a430bbb425fbf681770b4c2ceda983b840903f8
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2025-10-19 23:45:48 +0300

    fix; remove unused import

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/common/HeroSection"
 import CTA from "@/components/common/CTA"
-import Partner from "@/components/common/Partner"
 import WhyChoose from "@/components/common/WhyChoose"
 import Technologie from "@/components/common/Technologie"
 export default function Home() {
